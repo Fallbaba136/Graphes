0c5ffe5fb0666db624e9fdd6d6b3eaaaf256d8b0
Babacar Fall <Fallbaba136@gmail.com>
2026-02-11 00:57:41 +0100

    Réorganisation des TP (src/, nettoyage Git, suppression binaires)

---
+++ TP1/src/tp1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// ---------------------------------------
+// Université du Littoral Côte d'Opale
+// Licence Informatique 2e année
+// Module algorithmique 5 - Graphes - TP 1
+// ---------------------------------------
+
+// représentation d'un matrice d'adjacence
+struct MatriceAdjacence {
+  int *coef;// tableau contenant les (ordre x ordre) coefficients de la matrice
+  int ordre;// nombre de sommets du graphe
+};
+
+
+int main(int argc, char *argv[]){
+  
+
+ 
+  return 0;
+}
+
