e98868b462ad9b2ba7c34bf5935cec5b7e6adb5d
Babacar Fall <Fallbaba136@gmail.com>
2026-02-12 10:56:08 +0100

    continuité du TP2

--- TP2/src/tp2.cpp
+++ TP2/src/tp2.cpp
@@ -15,6 +15,10 @@ struct MatriceAdjacence
     Maillon* *lignes; //tab a allouer de taille ordre, representant less lignes de la matrice
 };
 
+//=====================
+// Exercice1:
+//=====================
+
 void creerMatrice(MatriceAdjacence *mat, int size){
     mat->ordre = size;
     mat->lignes = new Maillon*[size];
@@ -24,16 +28,20 @@ void creerMatrice(MatriceAdjacence *mat, int size){
     }
 }
 
+//=====================
+// Exercice2:
+//=====================
+
 bool chargerMatrice(char *nomFichier, MatriceAdjacence *mat){
     fstream fichier;
-    fichier.open(nomFichier);
+    fichier.open(nomFichier); // ouverture 
     if (!fichier.is_open())
     {
         cout << "Erreur d'ouverture du fichier" << endl;
         return false;
     }
     int ordre;
-    fichier >> ordre;
+    fichier >> ordre; // ordre stocke le premier chiffre du fichier qui est la taille spécifier dans le fichier en 1e ligne 
     creerMatrice(mat, ordre);
     for (int i = 0; i < ordre; i++)
     {
@@ -66,12 +74,16 @@ bool chargerMatrice(char *nomFichier, MatriceAdjacence *mat){
     return true;
 }
 
+//=========================
+// Exercice3:
+//=========================
+
 void afficherMatrice(MatriceAdjacence mat){
     for (int i = 0; i < mat.ordre; i++)
     {
         for (int j = 0; j < mat.ordre; j++)
         {
-            Maillon* tmp = mat.lignes[i];
+            Maillon* tmp = mat.lignes[i]; // ligne[] => tête du maillon
            int valeur = 0;
            while (tmp != nullptr)
            {
@@ -89,6 +101,102 @@ void afficherMatrice(MatriceAdjacence mat){
 }
 }
 
+//========================
+// Exercice4:
+//========================
+
+void effacerMatrice(MatriceAdjacence *M){
+    if (M->lignes == nullptr)
+    {
+        return;
+    }
+    
+    for (int i = 0; i < M->ordre; i++)
+    {
+        Maillon* tmp = M->lignes[i];
+        while (tmp != nullptr)
+        {
+             Maillon* aSupprimer = tmp;
+             tmp = tmp->suiv;
+            delete aSupprimer; // new = delete, malloc = free 
+        }       
+        M->lignes[i] = nullptr;
+    }
+    delete[] M->lignes;
+    M->lignes = nullptr;
+    M->ordre = 0;
+    
+}
+
+//==========================
+// Exercice5:
+//=========================
+
+int getCoef(MatriceAdjacence mat, int l, int c){ // l => indice ligne, c => indice colonne
+    int taille = mat.ordre;
+    if (mat.lignes == nullptr)
+    {
+        cout << "Matrice vide" << endl;
+        return 0;
+    }
+   Maillon* tmp = mat.lignes[l];
+   while (tmp != nullptr)
+   {
+        if (tmp->col == c)
+        {
+            return tmp->coef;
+        }
+        tmp = tmp->suiv;
+}
+   
+    return 0;
+    
+}
+
+
+//====================
+// Exercice6 
+//====================
+
+bool estComplet(MatriceAdjacence mat){
+    int taille = mat.ordre;
+    if (mat.lignes == nullptr)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < taille; i++)
+    {
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=========================
+// MAIN
+//========================= 
+
 int main(int argc, char* argv[]){
      if (argc < 2)
     {
@@ -99,6 +207,7 @@ int main(int argc, char* argv[]){
 
     bool file = chargerMatrice(argv[1], &matrice);
      afficherMatrice(matrice);
-
+     effacerMatrice(&matrice);
+     afficherMatrice(matrice);
      return 1;
 }
